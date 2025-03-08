ce03198f460d6bfdc23b7173495f8bdbd413fb80
George Mbajiaku <mbajiakugeorge@yahoo.com>
2025-03-08 17:58:48 +0000

    added get meme endpoint

--- api/index.py
+++ api/index.py
@@ -42,6 +42,30 @@ def get_recent_trends():
     )
     return jsonify(trends)
 
+@app.route("/meme/<meme_id>", methods=["GET"])
+def get_meme_details(meme_id):
+    # Fetch image from MongoDB
+
+    if not meme_id:
+        return "meme ID not found", 404
+    db = get_db_connection() 
+    memes_collection = db["memes"]  
+    single_meme = memes_collection.find_one({"_id": ObjectId(meme_id)}) 
+    if not single_meme:
+          return jsonify({"error": "Meme not found"}), 404 
+    single_meme["_id"] = str(single_meme["_id"])  # Convert ObjectId to string
+    # Return as image response
+    response_data = {
+        "topic": single_meme.get("topic"),
+        "captions": single_meme.get("captions"),
+        "meme_template": single_meme.get("meme_template"),
+        "timestamp": single_meme.get("timestamp"),
+        "source": single_meme.get("source"),
+        "_id": single_meme.get("_id"),
+        # Add other fields as needed
+    }
+    return jsonify({"meme": response_data })
+
 @app.route("/image/<meme_id>", methods=["GET"])
 def get_image(meme_id):
     # Fetch image from MongoDB
