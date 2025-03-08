09db134b9e8aaf51ba72b0ca6a16b305e09b35a6
George Mbajiaku <mbajiakugeorge@yahoo.com>
2025-03-08 09:56:29 +0000

    github actions scheduler

--- api/index.py
+++ api/index.py
@@ -6,14 +6,19 @@ app = Flask(__name__)
 
 # MongoDB connection
 MONGO_URI = os.getenv("MONGO_URI")
-client = MongoClient(MONGO_URI)
-db = client["meme_generator_db"]  # Database
-trends_collection = db["trending_topics"]  # Collection
-memes_collection = db["memes"]  # Collection
+
+def get_db_connection():
+    """Create a fresh MongoDB connection each request"""
+    client = MongoClient(MONGO_URI)
+    return client["meme_generator_db"] 
 
 @app.route("/memes", methods=["GET"])
 def get_memes():
     """Fetch all generated memes from the database."""
+
+    db = get_db_connection()
+    memes_collection = db["memes"]
+
     memes = list(memes_collection.find({}, {"_id": 0}))
     return jsonify(memes)
 
